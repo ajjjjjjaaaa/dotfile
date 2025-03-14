bd768942bda4cbaeb946d67b4d23f6a6b217a075
ajjjjjjaaaa <inabateshiki@gmail.com>
2025-03-14 15:37:48 +0800

    Add files via upload

---
+++ dot/nvim/lua/community.lua
@@ -0,0 +1,16 @@
+return {
+    "AstroNvim/astrocommunity",
+
+     -- colorscheme
+
+     { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+ 
+     -- languages
+
+     { import = "astrocommunity.pack.rust" },
+     { import = "astrocommunity.pack.python" }, 
+
+      -- tools
+      
+     --{ import = "astrocommunity.completion.copilot-lua" },
+}
